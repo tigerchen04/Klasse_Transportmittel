a851a4f42b997903eff409433cf622c71d9bdb96
tigerchen04 <104207588+tigerchen04@users.noreply.github.com>
2022-05-11 17:40:01 +0200

    Headerdatei (Methode nicht fertig)

---
+++ Transportmittel.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//Basisklasse:
+class Transportmittel {
+private:
+	double hoehe;
+	double breite;
+
+	//allgemeiner Konstruktor:
+	Transportmittel(double hoehe, double breite);
+
+public:
+	//Methode:
+	void bewegen(string richtung)
+	{
+		int x;
+
+	}
+};
