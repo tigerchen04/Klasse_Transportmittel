968cc9e757cf42e2cb0f207a60089902b468896c
tigerchen04 <104207588+tigerchen04@users.noreply.github.com>
2022-05-19 19:38:56 +0200

    abgeleitete Klassen Fahrrad, Rikscha und Auto

--- Transportmittel.h
+++ Transportmittel.h
@@ -63,4 +63,58 @@ public:
 
 	//allgemeiner Kontstruktor:
 	Wassertransportmittel(double hoehe, double breite, double bruttoregistertonnen) : Transportmittel(hoehe, breite);
-}
+};
+
+//abgeleitete Klasse Fahrrad:
+class Fahrrad : Landtransportmittel
+{
+private:
+	int anzahl_gaenge;
+
+public:
+	//Methoden:
+	void freihaendig_fahren(string name);
+	void schieben(double km);
+
+	//Gettermethode:
+	int getanzahl_gaenge();
+
+	//allgemeiner Kontstruktor:
+	Fahrrad(double hoehe, double breite, int radzahl, int anzahl_gaenge) : Landtransportmittel(hoehe, breite, radzahl);
+};
+
+//abgleitete Klasse Rikscha
+class Rikscha : Landtransportmittel
+{
+private:
+	string farbe;
+
+public:
+	//Methoden:
+	void ziehen(int anzahl_personen);
+	void schieben(double km);
+
+	//Gettermethode:
+	string getfarbe();
+
+	//allgemeiner Konstruktor:
+	Rikscha(double hoehe, double breite, int radzahl, string farbe) : Landtransportmittel(hoehe, breite, radzahl);
+};
+
+//abgeleitete Klasse Auto:
+class Auto : Landtransportmittel
+{
+private:
+	double spritverbrauch;
+
+public:
+	//Methoden:
+	void tanken(int liter);
+	void fahren(double km);
+
+	//Gettermethode:
+	double getspritverbrauch();
+
+	//allgemeiner Konstruktor:
+	Auto(double hoehe, double breite, int radzahl, double spritverbrauch) : Landtransportmittel(hoehe, breite, radzahl);
+};
